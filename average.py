f5635fa3e4cad117c8d79a791cc9e9e3ac1bfaf8
CatoJaloch <jaloch.cato@strathmore.edu>
2026-02-06 14:51:07 +0300

    updated files

---
+++ average.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np 
+import json
+import matplotlib.pyplot as plt
+#json loading
+with open("project1.json","r") as f:
+    data=json.load(f)
+#convert json to dataframe
+df=pd.json_normalize(data)
+#exporting to excel
+df.to_excel("project1_data.xlsx", index=False)
